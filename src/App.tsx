b6a21cda8060cb409e99db2fcc9ea02c10429776
descomplicandocompras <leosofty@hotmail.com>
2025-06-02 23:33:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ const App = () => {
     setShowResult(true);
     setResultado(resultadoFinal);
 
- fetch("https://v1.nocodeapi.com/descomplicacompras/google_sheets/MNWslNUwIcSWYVyy?tabId=Pagina1", {
+ fetch("https://v1.nocodeapi.com/descomplicacompras/google_sheets/MNWslNUwIcSWYVyy?tabId=Dados", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
